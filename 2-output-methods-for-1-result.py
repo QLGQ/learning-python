3f4d8ee4b53bd60fa160c06516ca11efc119eb70
QLGQ <519688681@qq.com>
2016-07-15 08:51:45 +0800

    Update 2-output-methods-for-1-result.py

--- 2-output-methods-for-1-result.py
+++ 2-output-methods-for-1-result.py
@@ -1,12 +1,2 @@
 title="The Meaning of Life"
 
-#the first method
->>> import myfile
->>> myfile.title
-
-#the second method 
->>> from myfile import title
->>> title
-
-#output
-'The Meaning of Life'
