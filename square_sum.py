f820f7710309a0b3eb90935610627eb6735e0c0f
qiang <qiang_wu@people2000.net>
2016-07-15 09:15:33 +0800

    add sum.py&square_sum.py&multify.py

---
+++ square_sum.py
@@ -0,0 +1,7 @@
+#calculate the value of 1^2+2^2+3^2+...+100^2
+sum=0
+i=1
+while i<101:
+   sum=sum+i*i
+   i=i+1
+print(sum)
---
+++ sum.py
@@ -0,0 +1,7 @@
+#calculate the value of 1+2+...+100
+sum=0
+i=1
+while i<101:
+   sum=sum+i
+   i=i+1
+print(sum)
