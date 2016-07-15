4f9fcd1da1aea3545fedcf6793e24c469fabfb1d
qiang <qiang_wu@people2000.net>
2016-07-15 17:14:20 +0800

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,16 @@
+#BMI
+#-*-coding:utf-8-*-
+
+height = 1.75
+weight = 80.5
+bmi = weight/height*height
+if bmi < 18.5:
+   print('过轻')
+elif bmi <= 25 and bmi >= 18.5:
+   print('正常')
+elif bmi <= 28 and bmi >= 26:
+   print('过重')
+elif bmi <= 32 and bmi >= 29:
+   print('肥胖')
+else:
+   print('严重肥胖')
